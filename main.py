364acb2e3d3ecd61bd010b4b9312c8da7850671b
mikeasta <m.astashonak@gmail.com>
2025-11-22 10:40:54 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+
+def main():
+    camera_id = 0
+    cap = cv.VideoCapture(camera_id)
+
+    # Getting camera resolution
+    w = cap.get(cv.CAP_PROP_FRAME_WIDTH)
+    h = cap.get(cv.CAP_PROP_FRAME_HEIGHT)
+
+    # Setting resolution
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, w * 1.2)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, h * 1.2)
+
+    while True:
+        if not cap.isOpened():
+            print(f"Cannot open camera {camera_id}")
+            exit()
+
+        else:
+            ret, frame = cap.read()
+
+            if not ret:
+                print("Cant recieve frame")
+                break
+            
+            cv.imshow("camera", frame)
+
+            if cv.waitKey(1) == ord("s"):
+                print("Camera stopped")
+                break
+
+    cap.release()
+
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
+
+        
